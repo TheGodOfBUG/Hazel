54c5e90be4d0bc07575705d22faf8621b40e43cf
Magic Cat <884041920@qq.com>
2024-12-10 11:01:13 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//Only for Hazel Application
+
+#include "Hazel/Application.h"
+
+// ---- Entry Point -----------------
+#include "Hazel/EntryPoint.h"
+
+// ----------------------------------
